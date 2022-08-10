9d13743548f507c36121dcb9899585ea07fb194e
ManasSarker <manas.sarker.moon@gmail.com>
2022-08-10 12:56:46 +0600

    first commit

---
+++ projects-2/app.js
@@ -0,0 +1,20 @@
+/* 
+
+Projects Requirements :
+
+- Change the background color by generating random hex color by clicking a button
+- Also display the hex code  to a disabled input field 
+
+
+
+*/
+
+// Step 1 -create onload handler
+
+window.onload = () => {
+  main();
+};
+
+function main() {}
+
+//step-2 random color generator function
